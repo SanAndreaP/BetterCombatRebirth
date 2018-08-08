d205e729e054e01c241d23636a6a9b1fdf72dde3
SanAndreasP <1294@live.de>
2018-08-08 21:59:51 +0200

    bumped version number

--- src/main/java/bettercombat/mod/util/Reference.java
+++ src/main/java/bettercombat/mod/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
   public static final String MOD_ID = "bettercombatmod";
   public static final String MOD_NAME = "Better Combat";
-  public static final String VERSION = "1.5.3";
+  public static final String VERSION = "1.5.4";
   public static final String CLIENT_PROXY_CLASS = "bettercombat.mod.client.ClientProxy";
   public static final String SERVER_PROXY_CLASS = "bettercombat.mod.util.CommonProxy";
 }
